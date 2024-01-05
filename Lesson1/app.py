734920568c836dcbc0c7b0326d5bddeca8ae28fe
Stan Bad <stankv@mail.ru>
2024-01-05 20:04:45 +0300

    добавлена вьюшка jackets

--- Lesson1/app.py
+++ Lesson1/app.py
@@ -23,6 +23,36 @@ def contact():
     return render_template('contact.html')
 
 
+@app.route('/jackets/')
+def jackets():
+    _jackets = [
+        {
+            "gender": "М",
+            "season": "осень",
+            "style": "классик",
+            "size": 50,
+            "brand": "Brand1",
+            "view": "Фото"
+        },
+        {
+            "gender": "М",
+            "season": "зима",
+            "style": "унисекс",
+            "size": 52,
+            "brand": "Brand2"
+        },
+        {
+            "gender": "Ж",
+            "season": "лето",
+            "style": "классик",
+            "size": 42,
+            "brand": "Brand3"
+        },
+    ]
+    context = {"jackets": _jackets}
+    return render_template('jackets.html', **context)
+
+
 @app.route('/jacket/')
 def jacket():
     return render_template('jacket.html')
