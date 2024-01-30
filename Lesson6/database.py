662c361ebfbdceacea1400da9bee7a4b987be81e
Stan Bad <stankv@mail.ru>
2024-01-30 18:44:41 +0300

    FastAPI. Подключение и отключение БД

---
+++ Lesson6/database.py
@@ -0,0 +1,19 @@
+import sqlalchemy
+import databases
+from sqlalchemy.pool import StaticPool
+from models import UserModel
+
+DATABASE_URL = "sqlite:///shop.db"
+db = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+
+async def startup():
+    await db.connect()
+    engine = sqlalchemy.create_engine(DATABASE_URL, echo=True, poolclass=StaticPool,
+                                      connect_args={"check_same_thread": False})
+    UserModel.metadata.create_all(engine)
+
+
+async def shutdown():
+    await db.disconnect()
